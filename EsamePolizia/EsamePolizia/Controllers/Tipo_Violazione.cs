cecbcc4485bd71839a27887081ecf8e542436058
Sachidu-2001 <fsachi0001@gmail.com>
2025-03-14 14:20:04 +0100

    commit

---
+++ EsamePolizia/EsamePolizia/Controllers/Tipo_Violazione.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EsamePolizia.Controllers
+{
+    public class Tipo_Violazione : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- EsamePolizia/EsamePolizia/Controllers/VerbaleController.cs
+++ EsamePolizia/EsamePolizia/Controllers/VerbaleController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EsamePolizia.Data;
+using EsamePolizia.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EsamePolizia.Controllers
 {
     public class VerbaleController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public VerbaleController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Verbali.ToListAsync());
+        }
+
+        [Route("add")]
+        public IActionResult Add()
         {
             return View();
         }
+
     }
 }
